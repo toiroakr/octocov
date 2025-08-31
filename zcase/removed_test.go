f228fada65bad6b2f8d0dc6bb9dbdb191a662e26
Akira HIGUCHI <akira_higuchi@tailor.tech>
2025-08-31 22:51:02 +0900

    before changes

---
+++ zcase/removed_test.go
@@ -0,0 +1,8 @@
+package zcase
+
+import "testing"
+
+func TestRemovedSum(t *testing.T) {
+	// Cover only one branch to generate partial coverage.
+	_ = RemovedSum(0, 1)
+}
---
+++ zcase/rename_old.go
@@ -0,0 +1,10 @@
+package zcase
+
+// RenamedValue exists as rename_old.go in report1 (A)
+// and will be renamed to rename_new.go in report2 (B).
+func RenamedValue(x int) int {
+	if x%2 == 0 {
+		return x / 2
+	}
+	return x * 2
+}
